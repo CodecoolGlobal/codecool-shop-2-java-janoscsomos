7efef978ec81f1fe11f61dc11e4268586e0bc228
janoscsomos <jstendec@gmail.com>
2021-09-21 15:53:01 +0200

    Add product descriptions and set prices.

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -28,8 +28,8 @@ public class ProductController extends HttpServlet {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productService.getProductCategory(6));
-        context.setVariable("products", productService.getProductsForCategory(6));
+        context.setVariable("category", productService.getProductCategory(8));
+        context.setVariable("products", productService.getProductsForCategory(8));
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
         // params.put("category", productCategoryDataStore.find(1));
