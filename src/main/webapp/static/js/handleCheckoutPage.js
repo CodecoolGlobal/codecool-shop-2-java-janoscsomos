df4abb00a3da34a8d4c144a44d61f6b443ce2e54
janoscsomos <jstendec@gmail.com>
2021-09-22 17:41:54 +0200

    Implement admin log

--- src/main/webapp/static/js/handleCheckoutPage.js
+++ src/main/webapp/static/js/handleCheckoutPage.js
@@ -17,8 +17,17 @@ function fieldHandler(e) {
     logOutput.push(`${e.target.id} input occurred`);
 }
 
+function fieldChecker() {
+    let fieldsAtCheckout = document.querySelectorAll(".form-control");
+    logOutput.push("FIELD STATUSES ON CHECKOUT EVENT")
+    for (let field of fieldsAtCheckout) {
+        let fieldStatus = field.id + " length: " + field.value.length.toString();
+        logOutput.push(fieldStatus);
+    }
+}
 
 function checkoutButtonHandler(e) {
+    fieldChecker();
     apiGet("http://0.0.0.0:8888/api/adminlog", logOutput);
 }
 
