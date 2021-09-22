ab39e937c86bc268d6d26ad46d34e9deff8ec98b
janoscsomos <jstendec@gmail.com>
2021-09-22 17:17:54 +0200

    Format log output

--- src/main/java/com/codecool/shop/api/LogIO.java
+++ src/main/java/com/codecool/shop/api/LogIO.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class LogIO {
 
@@ -13,12 +14,23 @@ public class LogIO {
     public void exportLog(String logOutput)
             throws IOException, ClassNotFoundException {
 
+        String[] splitLog= logOutput.split(",");
+        StringBuilder formattedLog = new StringBuilder();
         LocalDateTime date = LocalDateTime.now();
+
+        formattedLog.append("CHECKOUT LOG START AT " + date.toString() + "\n");
+        for (int i = 0; i < splitLog.length; i++) {
+            formattedLog.append(splitLog[i]);
+            formattedLog.append("\n");
+        }
+        formattedLog.append("CHECKOUT LOG END");
+
+
         FileOutputStream fileOutputStream
                 = new FileOutputStream(date.toString() +".json");
         ObjectOutputStream objectOutputStream
                 = new ObjectOutputStream(fileOutputStream);
-        objectOutputStream.writeObject(logOutput);
+        objectOutputStream.writeObject(formattedLog.toString());
         objectOutputStream.flush();
         objectOutputStream.close();
     }
