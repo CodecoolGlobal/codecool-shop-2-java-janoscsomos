e8beca7956cae5bd4c3c9c6513ab876f124952aa
dorogimate <dorogimate6@gmail.com>
2021-09-23 17:31:38 +0200

    refactor add route

--- src/main/java/com/codecool/shop/api/SessionAddJsonServlet.java
+++ src/main/java/com/codecool/shop/api/SessionAddJsonServlet.java
@@ -1,15 +1,5 @@
 package com.codecool.shop.api;
 
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
-import com.codecool.shop.model.ShoppingCart;
-import com.codecool.shop.service.ProductService;
-import com.google.gson.Gson;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,9 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Map;
 
 
@@ -29,16 +17,8 @@ public class SessionAddJsonServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-
-   /*
-        ShoppingCart cart = (ShoppingCart) request.getSession().getAttribute("cart");
-        cart.addToCart("Banana", 2);
-
-    */
         HttpSession session = request.getSession();
-        if (session.isNew()) {
+        if (session.getAttribute("shoppingCart") == null) {
             shoppingCart = new HashMap<String, Integer>();
             session.setAttribute("shoppingCart", shoppingCart);
         } else {
