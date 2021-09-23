23151cb3aa0e1395fcc0be5628d16bd98811acf1
moloch291 <danielkomjati1@gmail.com>
2021-09-24 00:48:08 +0200

    design confirm page

--- src/main/webapp/static/js/handleConfirmationPage.js
+++ src/main/webapp/static/js/handleConfirmationPage.js
@@ -1,18 +1,22 @@
 getCart().then(data => {
     let idP = document.getElementById("order-id");
     idP.textContent = "Order ID: " + localStorage.getItem("orderId");
-    let detailsDiv = document.getElementById("order-details");
+    let detailsDiv = document.querySelector("#product-list");
     let totalSum = 0;
+    let prodNumber = 0;
+    let newProdList = "";
     for (let item of data) {
-        let itemName = document.createElement('p');
-        itemName.textContent = item['amount'] + " " + item['name'];
-        detailsDiv.appendChild(itemName);
+        newProdList += item['amount'] + " " + item['name'] + "(s), ";
+        prodNumber++;
     }
+    detailsDiv.textContent = newProdList.substring(0, newProdList.length - 2);
     for (let item of data) {
         totalSum += parseFloat(item['defaultPrice']) * parseFloat(item['amount']);
     }
     let sum = document.createElement('p');
-    sum.textContent = totalSum.toString() + " USD"
+    document.querySelector("#item-number").textContent = prodNumber.toString() + " items";
+    document.querySelector(".subtotal").textContent = "$" + totalSum.toFixed(2).toString();
+    document.querySelector(".total-price").textContent = "$" + (totalSum + 1.80).toFixed(2).toString();
     detailsDiv.appendChild(sum);
 }).then(clearCart);
 
