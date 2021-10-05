d4e247e82f470418c4217c4592e15f0f24323f15
moloch291 <danielkomjati1@gmail.com>
2021-10-05 10:26:06 +0200

    add mockito dependency to pom.xml and create test fields and @BeforeEach

--- src/test/java/com/codecool/shop/service/ProductServiceTest.java
+++ src/test/java/com/codecool/shop/service/ProductServiceTest.java
@@ -1,13 +1,31 @@
 package com.codecool.shop.service;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductServiceTest {
+    private ProductService productService;
+    private ProductDao productDao;
+    private ProductCategoryDao categoryDao;
+    private SupplierDao supplierDao;
+
+    @BeforeEach
+    void build() {
+        ProductDao productDao = Mockito.mock(ProductDao.class);
+        ProductCategoryDao categoryDao = Mockito.mock(ProductCategoryDao.class);
+        SupplierDao supplierDao = Mockito.mock(SupplierDao.class);
+        productService = new ProductService(productDao, categoryDao, supplierDao);
+    }
 
     @Test
-    void getProductCategory_returnsRightCategoryWithRightId() {
+    void getProductCategory_returnsCategoryWithRightId() {
+
     }
 
     @Test
