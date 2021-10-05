406ecdc300f7883d9bf4c28ae5d556a822b6980d
Sztomi8 <szilagyitamas94@gmail.com>
2021-10-05 15:09:27 +0200

    implement recieving registration data in the backend

---
+++ src/main/java/com/codecool/shop/api/RegistrationServlet.java
@@ -0,0 +1,18 @@
+package com.codecool.shop.api;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "hackerNewsServlet", urlPatterns = {"/save-registration"})
+public class RegistrationServlet extends javax.servlet.http.HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(request.getParameter("email"));
+
+    }
+}
