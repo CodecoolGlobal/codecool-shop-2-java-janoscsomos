31582e0a9148fa4da001481115e647bf9b4cfa71
moloch291 <danielkomjati1@gmail.com>
2021-10-05 14:56:42 +0200

    implement test for product search by category

--- src/test/java/com/codecool/shop/service/ProductServiceTest.java
+++ src/test/java/com/codecool/shop/service/ProductServiceTest.java
@@ -3,11 +3,16 @@ package com.codecool.shop.service;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,22 +32,20 @@ class ProductServiceTest {
         productDaoMem = ProductDaoMem.getInstance();
         supplierDaoMem = SupplierDaoMem.getInstance();
 
-        productService = new ProductService(
-                productDaoMem,
-                categoryDaoMem,
-                supplierDaoMem
-        );
+        productService = new ProductService(productDaoMem, categoryDaoMem, supplierDaoMem);
     }
 
     @Test
     void getProductCategory_returnsCategoryWithExistingId() {
+        // Add mock category to category DAO:
         categoryDaoMem.add(mockCategory);
-
+        // Assert:
         assertEquals(mockCategory, productService.getProductCategory(0));
     }
 
     @Test
-    void getProductsForSupplier_throwsIllegalArgExceptionWithNonExistentId() {
+    void getProductCategory_throwsIllegalArgExceptionWithNonExistentId() {
+        // Assert for exception check:
         assertThrows(
                 IllegalArgumentException.class,
                 () -> productService.getProductCategory(1000)
@@ -50,7 +53,22 @@ class ProductServiceTest {
     }
 
     @Test
-    void getProductsForCategory() {
+    void getProductsForCategory_returnsRightProductsWithCategory() {
+        // Add mock category to category DAO:
+        categoryDaoMem.add(mockCategory);
+        // Create and add mock Product to product DAO:
+        Product mockProduct = Mockito.mock(Product.class);
+        Mockito.when(mockProduct.getProductCategory()).thenReturn(mockCategory);
+        productDaoMem.add(mockProduct);
+        // Created expectation:
+        List<Product> expected = new LinkedList<>();
+        expected.add(mockProduct);
+        // Assert:
+        assertEquals(expected, productService.getProductsForCategory(0));
+    }
+
+    @Test
+    void getProductsForSupplier() {
     }
 
     @Test
@@ -63,6 +81,11 @@ class ProductServiceTest {
 
     @Test
     void getAllProducts() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        categoryDaoMem.remove(0);
     }
 
     @AfterAll
