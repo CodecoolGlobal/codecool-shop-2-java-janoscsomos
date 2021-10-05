43905a4830c883a756adef909ed586af67dabb43
moloch291 <danielkomjati1@gmail.com>
2021-10-05 10:16:13 +0200

    Create package structure and skeleton for tests.

---
+++ src/test/java/com/codecool/shop/service/ProductServiceTest.java
@@ -0,0 +1,36 @@
+package com.codecool.shop.service;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void getProductCategory_returnsRightCategoryWithRightId() {
+    }
+
+    @Test
+    void getProductCategory_throwsIllegalArgumentExceptionWithWrongId() {
+    }
+
+    @Test
+    void getProductsForSupplier() {
+    }
+
+    @Test
+    void getProductsForCategory() {
+    }
+
+    @Test
+    void getProductsByName() {
+    }
+
+    @Test
+    void getProductByName() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+}
