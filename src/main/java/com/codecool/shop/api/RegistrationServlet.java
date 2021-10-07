f28badeb35cc19a2905a2ceff2dc373255259be3
moloch291 <danielkomjati1@gmail.com>
2021-10-07 10:35:51 +0200

    finalize registartion_and_login.html

--- src/main/java/com/codecool/shop/api/RegistrationServlet.java
+++ src/main/java/com/codecool/shop/api/RegistrationServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "hackerNewsServlet", urlPatterns = {"/save-registration"})
+@WebServlet(name = "hackerNewsServlet", urlPatterns = {"/registration"})
 public class RegistrationServlet extends javax.servlet.http.HttpServlet {
 
     @Override
